07cdb1dc585bea575534c384d81b136c256c2d56
TeodorKovac <teodor.kovac95@gmail.com>
2022-12-06 21:51:11 +0100

    final 1

--- fourierDescriptors.py
+++ fourierDescriptors.py
@@ -1,10 +1,12 @@
 import cv2 as cv
 import numpy as np
 
+
 def findDescriptor(img):
     """ findDescriptor(img) finds and returns the
     Fourier-Descriptor of the image contour"""
     contour = []
+    img = cv.convertScaleAbs(img)
     contour, hierarchy = cv.findContours(img, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE, contour)
     contour_array = contour[0][:, 0, :]
     contour_complex = np.empty(contour_array.shape[:-1], dtype=complex)
@@ -12,3 +14,24 @@ def findDescriptor(img):
     contour_complex.imag = contour_array[:, 1]
     fourier_result = np.fft.fft(contour_complex)
     return fourier_result
+
+
+def truncate_descriptor(descriptors, degree):
+    """this function truncates an unshifted fourier descriptor array
+    and returns one also unshifted"""
+    descriptors = np.fft.fftshift(descriptors)
+    center_index = len(descriptors) / 2
+    descriptors = descriptors[
+                  round(center_index - degree / 2):round(center_index + degree / 2)]
+    descriptors = np.fft.ifftshift(descriptors)
+    return descriptors
+
+
+def normovani(descriptors):
+    abs1 = abs(descriptors[1])
+    for i in range(2, len(descriptors)):
+        descriptors[i] = abs(descriptors[i]) / abs1
+        descriptors[i] = descriptors[i].real
+
+    descriptors = descriptors[2:]
+    return descriptors
--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from scipy.ndimage import gaussian_filter
 # snadno rozpoznatelná od pozadí. (použil jsem fotky digitálního teploměru krbu - folder framesEasy)
 #       - pokud by byla čísla orotovaná, tak např. pomocí horizontální projekce zjistíme úhel
 #       nebo openCV minAreaRect() zjistí uhel rotace nejmenšího opsaného čtyřuhelníku
-#       nebo metoda Fourierovy deskriptory...
 # 2) Převedení obrázku do grayscale, drobná eliminace šumu pomoci konvoluce Gaussovo filtrem.
 # 3) Poté převedení do black&white prahováním.
 #       - dalo by se použít metoda pro hledání prahu (Otsu?)...
@@ -22,31 +21,57 @@ from scipy.ndimage import gaussian_filter
 #       - Fourierovy deskriptory
 #       - Neuronové sítě v případě trénovací množiny
 
+MIN_DESCRIPTOR = 10
+
+
 if __name__ == '__main__':
 
     img = cv.imread("framesEasy/001.png")
     grayimg = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     (thresh, img) = cv.threshold(grayimg, 240, 255, cv.THRESH_BINARY)
     (cisla, kolikCisel) = pf.segmentace(img)
-    A = cisla[0]
-    deskriptory1 = fd.findDescriptor(A)
-    print(deskriptory1)
-
-    for fileloc in glob.iglob(os.getcwd() + "/framesEasy/*.png"):
-        img = cv.imread(fileloc)
-        grayimg = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-        # Eliminace šumu pomocí morfologie, konvoluce s Gaussovo filtrem
-        grayimg = gaussian_filter(grayimg, sigma=2)
-        # Prahování nahodilou hodnotou 240, jinak možno použít např. Otsu(?)
-        (thresh, img) = cv.threshold(grayimg, 240, 255, cv.THRESH_BINARY)
-        print(fileloc)
-        # Funkce pro segmentaci a rozdělení jednotlivých čísel
-        (cisla, kolikCisel) = pf.segmentace(img)
-        # Vim, že budou tři, jinak iterace přes počet čísel
-        A = cisla[0]
+    for i in range(0, 2):
+        A = cisla[i]
         digit1 = pf.recognizeNumber(A)
-        B = cisla[1]
-        digit2 = pf.recognizeNumber(B)
-        C = cisla[2]
-        digit3 = pf.recognizeNumber(C)
-        print(digit1, digit2, digit3)
+        deskriptory1 = fd.findDescriptor(A)
+        deskriptory1 = deskriptory1[0:MIN_DESCRIPTOR]
+        deskriptory1 = fd.normovani(deskriptory1)
+        #deskriptory1 = fd.truncate_descriptor(deskriptory1, MIN_DESCRIPTOR)
+        print(digit1)
+        print(deskriptory1)
+    #
+    print("hotovo 001")
+    img = cv.imread("framesEasy/002.png")
+    grayimg = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    (thresh, img) = cv.threshold(grayimg, 240, 255, cv.THRESH_BINARY)
+    (cisla, kolikCisel) = pf.segmentace(img)
+    for j in range(0, 2):
+        A = cisla[j]
+        digit1 = pf.recognizeNumber(A)
+        deskriptory1 = fd.findDescriptor(A)
+        deskriptory1 = deskriptory1[0:MIN_DESCRIPTOR]
+        deskriptory1 = fd.normovani(deskriptory1)
+        #deskriptory1 = fd.truncate_descriptor(deskriptory1, MIN_DESCRIPTOR)
+        print(digit1)
+        print(deskriptory1)
+    #
+    print("hotovo 002")
+
+    #for fileloc in glob.iglob(os.getcwd() + "/framesEasy/*.png"):
+     #   img = cv.imread(fileloc)
+      #  grayimg = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        # Eliminace šumu pomocí morfologie, konvoluce s Gaussovo filtrem
+       # grayimg = gaussian_filter(grayimg, sigma=2)
+        # Prahování nahodilou hodnotou 240, jinak možno použít např. Otsu(?)
+       # (thresh, img) = cv.threshold(grayimg, 240, 255, cv.THRESH_BINARY)
+       # print(fileloc)
+        # Funkce pro segmentaci a rozdělení jednotlivých čísel
+       # (cisla, kolikCisel) = pf.segmentace(img)
+        # Vim, že budou tři, jinak iterace přes počet čísel
+       # A = cisla[0]
+       # digit1 = pf.recognizeNumber(A)
+       # B = cisla[1]
+       # digit2 = pf.recognizeNumber(B)
+       # C = cisla[2]
+       # digit3 = pf.recognizeNumber(C)
+       # print(digit1, digit2, digit3)
